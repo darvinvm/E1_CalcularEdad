ca10a3bb9ae37f717430b0d497250ae6c58de7e9
ghtdeveloper <martinezfriasedison@gmail.com>
2020-04-01 17:22:32 -0400

    Primer commit, se agregaron los temas, se creo la          funcion para calcular la edad, ademas de modificaciones a la vista          (layout).

--- app/src/main/java/com/alluberes/e1_calcularedad/MainActivity.java
+++ app/src/main/java/com/alluberes/e1_calcularedad/MainActivity.java
@@ -2,47 +2,97 @@ package com.alluberes.e1_calcularedad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
 
     /// TODO (11) Declarar objeto tipo EditText de "nombre_persona"
-
+    EditText txtnombrePersona;
     /// TODO (12) Declarar objeto tipo DatePicker de "date_of_birth"
-
+    DatePicker datePicker;
     /// TODO (13) Declarar objeto tipo Button de "calcular_edad"
-
+    Button btnCalcularEdad;
     /// TODO (14) Declarar objeto tipo EditText de "edad_actual"
+    TextView textViewDatos;
 
+    //Fecha Actual
+     private int year;
 
-
+    @SuppressLint("SimpleDateFormat")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         /// TODO (15) Relacionar elemento de layout para nombre de persona con variable
+        //Se realiza el cast con las vistas del layout
+        //toolbar = findViewById(R.id.toolbar);
+        txtnombrePersona = findViewById(R.id.txtNombre);
+        datePicker = findViewById(R.id.datePicker);
+        btnCalcularEdad = findViewById(R.id.btnCalcularEdad);
+        textViewDatos = findViewById(R.id.textMostrarDatos);
 
-        /// TODO (16) Relacionar elemento de layout para fecha de nacimiento con variable
+        //Se inicializan los objetos
+        //Objetos
+        Calendar calendar = Calendar.getInstance();
+        //Se obtiene el year actual del dispositivo
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY");
+        //Se  asigna el valor a nuestra variable year
+        year = Integer.parseInt(simpleDateFormat.format(calendar.getTime()));
 
-        /// TODO (17) Relacionar elemento de layout para accion de calcular edad con variable
+        /// TODO (20) Asignar accion para el evento setOnClickListener del
+        // elemento de boton que calcula la edad
+        /*
+            Metodo definido para mostrar la edad de la persona
+         */
+        btnCalcularEdad.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
 
-        /// TODO (18) Relacionar elemento de layout para mostrar la edad actual con variable
 
+                //Si el nombre esta vacio no voy a mostra la edad e envio una
+                // notificacion al usuario
+                if(txtnombrePersona.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),
+                            "El campo nombre no puede estar vacio",
+                            Toast.LENGTH_SHORT).show();
+                }
 
-        /// TODO (20) Asignar accion para el evento setOnClickListener del elemento de boton que calcula la edad
-        /// en el evento onClick se debe tomar el valor del DatePicker que indica la fecha de nacimiento,
-        /// e invocar la funcion que se crea en el paso 19 para calcular la edad
-        /// y mostrar en el elemento del layout para mostrar edad un texto en el siguiente orden
-        /// [nombre de la persona] tiene [edad] años de edad
-        /// debe sustituir los valores que estan en los corchetes por los valores de los elementos del layout
-
-    }
+                else
+                {
+                    textViewDatos.setText(txtnombrePersona.getText().toString() +"  "+"tiene"+ "  "+
+                            calcularEdad(datePicker.getYear(),datePicker.getDayOfMonth()
+                                    ,datePicker.getMonth()) + " años de edad");
+                }
+            }
+        });
+    }//Fin del metodo onCreate
 
     /// TODO (19) Crear funcion para calcular edad. Ha de recibir como parametro año, mes y dia
-    /// internamente ha de sacar la diferencia entre la fecha actual y la fecha dada y retornara el valor
+    /// internamente ha de sacar la diferencia entre la fecha actual y la fecha dada y retornara
+    // el valor
+
+    /*
+        Funcion definida para calcular la edad de la persona
+     */
+    public int calcularEdad(int yearSelecionado, int mes, int dia)
+    {
+        return year - yearSelecionado;
+    }//Fin de la funcion calcularEdad
 
 
-}
+}//Fin de la class MainActivity
